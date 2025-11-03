60cd2bc92160a1408ba1752c1326bb1caf21e23e
kcersing <wt4@live.cn>
2025-11-03 17:30:20 +0800

    Revert "CRM"
    This reverts commit 033d274fcb2f79a6f318b638556760bd72aa4b6d.

--- idl/rpc/member.thrift
+++ idl/rpc/member.thrift
@@ -21,7 +21,6 @@ struct Member {
 
 }
 
-
 struct CreateMemberReq{
     1:optional string username="",
     2:optional string password="",
@@ -51,17 +50,12 @@ struct ChangePasswordReq {
 service MemberService  {
 
      MemberResp CreateMember(1: CreateMemberReq req)
-      base.BaseResp DeleteMember(1: base.IdReq req)
+     base.NilResponse DeleteMember(1: base.IdReq req)
      MemberResp UpdateMember(1: UpdateMemberReq req)
      MemberResp GetMember(1: base.IdReq req)
      MemberListResp GetMemberList(1: GetMemberListReq req)
 
      MemberResp LoginMember(1: base.CheckAccountReq req)
-     base.BaseResp ChangePassword(1: ChangePasswordReq req)
-
-     // 商机
-     // 跟进计划
-     // 跟进记录
-
+     base.NilResponse ChangePassword(1: ChangePasswordReq req)
 
 }
